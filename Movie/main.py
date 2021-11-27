ba69f9463118b45ffc5b038401eec317651ff642
PlutoSenthil <71826463+PlutoSenthil@users.noreply.github.com>
2021-11-27 23:24:53 +0530

    comment main function

--- Movie/main.py
+++ Movie/main.py
@@ -97,8 +97,8 @@ https://tiangolo.medium.com/introducing-fastapi-fdc1206d453f
 You will also need an ASGI server for production such as uvicorn
 """
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
 #     # review = "This movie was exactly what I wanted in a Godzilla vs Kong movie. It's big loud, brash and dumb, in the best ways possible. It also has a heart in a the form of Jia (Kaylee Hottle) and a superbly expressionful Kong. The scenes of him in the hollow world are especially impactful and beautifully shot/animated. Kong really is the emotional core of the film (with Godzilla more of an indifferent force of nature), and is done so well he may even convert a few members of Team Godzilla."
 #     # keys = {"review": review}
 #     # prediction = r.get("http://127.0.0.1:8000/predict-review/", params=keys)
